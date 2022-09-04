1533cfbb9c49849a9a2b489dc8a580aa93a2b424
Mulloa90 <mario90gm@icloud.com>
2022-09-04 13:24:03 -0700

    Updated seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,24 @@
-CREATE database employee_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
 USE employee_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
-    department_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
 );
 CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 0) NOT NULL,
-    department_id INT,
-    PRIMARY KEY (id)
+    department_id INT NOT NULL,
+    FOREIGN KEY department_id REFERENCES department(id) ON DELETE CASCADE
 );
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY (id)
-);
+    FOREIGN KEY role_id REFERENCES role(id) ON DELETE CASCADE
+    FOREIGN KEY manager_id REFERENCES employee(id) ON DELETE SET NULL);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,37 +1,16 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Mario', 'Ulloa', 1, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Herbie', 'Smith', 2, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('James', 'Peach', 3, 2);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Dwight', 'Howard', 4, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Jason', 'Terry', 3, 2);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Joe', 'Dirt', 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Cheech', 'Chong', 5, null);
+USE employee_db;
 
-INSERT INTO department (department_name)
-VALUES ('Management')
-INSERT INTO department (department_name)
-VALUES ('Developers')
-INSERT INTO department (department_name)
-VALUES ('IT')
-INSERT INTO department (department_name)
-VALUES ('HR')
-INSERT INTO department (department_name)
-VALUES ('Intern');
+INSERT INTO department (name)
+VALUES ('Management'), ('Developers'), ('IT'), ('HR'), ('Intern');
+
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('General Manager', 450000, 1);
-INSERT INTO role (title, salary, department_id)
-VALUES ('Software Developer', 150000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUES ('IT Lead', 120000, 3);
-INSERT INTO role (title, salary, department_id)
-VALUES ('HR Specialist', 450000, 4);
-INSERT INTO role (title, salary, department_id)
-VALUES ('Software Developer Intern', 70000, 5);
+VALUES ('General Manager', 450000, 1),('Software Developer', 150000, 2), 
+('IT Lead', 120000, 3),('HR Specialist', 450000, 4),
+('Software Developer Intern', 70000, 5);
 
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Mario', 'Ulloa', 1, 1),('Herbie', 'Smith', 2, null),('James', 'Peach', 3, 2),
+('Dwight', 'Howard', 4, null),('Jason', 'Terry', 3, 2),
+('Joe', 'Dirt', 2, 1),('Cheech', 'Chong', 5, null);
