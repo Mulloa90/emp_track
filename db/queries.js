d60cdff2d3f1e5d04f14966f8d384c3db501342d
Mulloa90 <mario90gm@icloud.com>
2022-09-08 22:07:24 -0700

    updates add role

--- db/queries.js
+++ db/queries.js
@@ -3,9 +3,31 @@ const connection = require("./connection");
 class Store {
   constructor(connection) {
     this.connection = connection;
-  } 
+  }
   getAllDepartments() {
-    return this.connection.promise().query("SELECT * from department;")
+    return this.connection.promise().query("SELECT * from department;");
+  }
+  getAllRoles() {
+    return this.connection.promise().query("SELECT * from role;");
+  }
+
+  getAllEmployees() {
+    return this.connection.promise().query("SELECT * from employee;");
+  }
+  createNewdepartment(departmentName) {
+    return this.connection
+      .promise()
+      .query("INSERT INTO department SET ?", departmentName);
+  }
+  createNewEmployee(employeeData) {
+    return this.connection
+      .promise()
+      .query("INSERT INTO employee SET ?", employeeData);
+  }
+  createNewRole(RoleData) {
+    return this.connection
+      .promise()
+      .query("INSERT INTO role SET ?", RoleData);
   }
 }
-module.exports = new Store(connection)
+module.exports = new Store(connection);
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require("inquirer");
 require("console.table");
 const util = require("util");
+const { query } = require("./db/connection");
+const { connection } = require("./db/queries");
+
 const store = require("./db/queries");
 
 function init() {
@@ -10,13 +13,35 @@ function init() {
         type: "list",
         name: "selection",
         message: "What would you like to do?",
-        choices: ["View all departments"],
+        choices: [
+          "View all departments",
+          "Add a department",
+          "View All Roles",
+          "View All Employees",
+          "Add new role",
+          "Quit",
+        ],
       },
     ])
     .then((response) => {
       switch (response.selection) {
         case "View all departments":
           viewAllDepartments();
+          break;
+        case "Add a department":
+          createNewdepartment();
+          break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+        case "Add New Role":
+          addNewRole();
+          break;
+        case "Quit":
+          process.exit();
       }
     });
 }
@@ -24,5 +49,86 @@ init();
 function viewAllDepartments() {
   store.getAllDepartments().then(([departments]) => {
     console.table(departments);
+    init();
+  });
+}
+function viewAllRoles() {
+  store.getAllRoles().then(([roles]) => {
+    console.table(roles);
+    init();
+  });
+}
+function viewAllEmployees() {
+  store.getAllEmployees().then(([employees]) => {
+    console.table(employees);
+    init();
+  });
+}
+function createNewdepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "Department Name",
+        message: "Whats is the new departments name?",
+      },
+    ])
+    .then((response) => {
+      store
+        .createNewdepartment({ name: response["Department Name"] })
+        .then((response) => {
+          console.log(`Added ${response.name} to the db`);
+        })
+        .then(() => {
+          init();
+        });
+    });
+}
+function addNewRole() {
+  connection.query(`SELECT * FROM department;`, function (err, res) {
+    if (err) throw err;
+    const departments = [];
+    for (let i = 0; i <= res.length - 1; i++) {
+      departments.push(res[i].name);
+    }
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "Role name",
+          message: "What is the name of the role?",
+        },
+        {
+          type: "input",
+          name: "Salary",
+          message: "What is the salary of the role?",
+        },
+        {
+          type: "lists",
+          name: "department",
+          message: "Which department does this role belong to?",
+          choices: departments,
+        },
+      ])
+      .then((response) => {
+        let id;
+        for (let i = 0; i <= res.length - 1; i++) {
+          if (res[i].name === response["department"]) {
+            id = res[i].id;
+          }
+        }
+        store
+          .createNewRole({
+            title: response["Role Name"],
+            salary: response["Salary"],
+            department_id: response["departments"],
+          })
+          .then((response) => {
+            console.log(`Added ${response.title} to the db`);
+          })
+          .then(() => {
+            init();
+          });
+      });
   });
 }
